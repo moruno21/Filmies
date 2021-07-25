23ced1846087c546b5c762391aa0501162e35b94
moruno21 <amoruno21@gmail.com>
2021-07-25 17:58:06 +0200

    series service and interface created

---
+++ frontend/src/app/models/series.ts
@@ -0,0 +1,9 @@
+export interface Series {
+  id?: number;
+  title?: string;
+  description?: string;
+  image?: string;
+  seasons?: number;
+  episodes?: number;
+  date?: string;
+}
--- frontend/src/app/services/series.service.ts
+++ frontend/src/app/services/series.service.ts
@@ -1,9 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Series } from '../models/series';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SeriesService {
+  api_link = 'http://localhost:3000/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getOneSeries(id: string): Observable<Series> {
+    return this.http.get(`${this.api_link}/series/${id}`);
+  }
+
+  getSeries(): Observable<Series> {
+    return this.http.get(`${this.api_link}/series`);
+  }
+
+  postSeries(series: series): Observable<Series> {
+    return this.http.post(`${this.api_link}/series`, series);
+  }
+
+  deleteSeries(id: string): Observable<Series> {
+    return this.http.delete(`${this.api_link}/series/${id}`);
+  }
+
+  updateSeries(id: string, series: Series): Observable<Series> {
+    return this.http.put(`${this.api_link}/series/${id}`, series);
+  }
 }
